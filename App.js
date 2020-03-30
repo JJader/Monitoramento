aad232af64aa308dae4769fd9938ba4248311744
Jamisson Jader <jjader03@gmail.com>
2020-03-30 19:55:18 -0300

    adicionando função de otimização de memória

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 import Drawer from './src/screens/drawerNavigation'
-
+import {useScreens} from 'react-native-screens'
+useScreens();
 globalThis.URL_API = 'http://192.168.1.160:5000'
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
 
   
   render() {
+    
     return (
       <Drawer/>
     );
